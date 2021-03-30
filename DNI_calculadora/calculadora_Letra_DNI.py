24d089ff817e624d7b214709aae7ae5beaa4ab2a
AntoniPizarro <apizarro@cifpfbmoll.eu>
2021-03-30 11:15:20 +0200

    feat: Interacción por consola
    El programa solicita al usuario datos por consola

--- DNI_calculadora/calculadora_Letra_DNI.py
+++ DNI_calculadora/calculadora_Letra_DNI.py
@@ -1,3 +1,10 @@
+import os
+
 def calculadora(num):
     letras = "TRWAGMYFPDXBNJZSQVHLCKE"
     return letras[num % 23]
+
+if __name__ == "__main__":
+    num = int(input())
+    print(calculadora(num))
+    os.system("pause")
